1757a25e8816db14dc9d8df7d4c38d8c212cf810
tussle <gaming124@gmail.com>
2014-03-20 12:30:00 +0000

    PageAdder added

--- src/java/Base/PageAdder.java
+++ src/java/Base/PageAdder.java
@@ -6,17 +6,33 @@
 
 package Base;
 
+import java.io.File;
+
 /**
  *
  * @author Thomas
  */
 public class PageAdder {
+    private User user;
+    private String pagename;
+    private String PATH = "C:/Users/as11.CS-DOMAIN/Downloads/p3-master/build/web/";
+    
     public PageAdder(User user, String name){
-        
+        this.user=user;
+        this.pagename=name;
+        PATH+=user.getCharityName()+"/";
     }
     
     public boolean addPage(){
+        File f= new File(PATH+pagename); 
         
+        try{
+            if(f.createNewFile()){
+                
+            }
+        }catch(Exception e){
+            
+        }
         return true;
     }
 }
